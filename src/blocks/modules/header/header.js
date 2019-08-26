33857f6e455ce87316b5ff8cf431b7dd1e91ddcd
Aleksei <super.rybko@mail.ru>
2019-08-26 17:05:23 +0300

    Add all blocks tto Video block

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,6 @@
+new Vue({
+  el: '#select-vue',
+  data: {
+    selected: ''
+  }
+})
---
+++ src/blocks/modules/price/price.js
@@ -0,0 +1,4 @@
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+document.getElementById('count-generated').innerHTML = getRandom(50, 250);
